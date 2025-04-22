4e86d495a51a3ecb65d71c08d62bf5c7ca682b5c
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2025-04-22 10:51:39 -0300

    Update professor.h

--- professor.h
+++ professor.h
@@ -1,18 +1,20 @@
 #ifndef PROFESSOR_H
 #define PROFESSOR_H
 
+#define TAM_PROFESSOR 3
+
 typedef struct {
-    int id;
     char nome[100];
-    char sexo;
-    char dataNascimento[11];
-    int disciplinas[10]; // Disciplinas que o professor leciona
-    int qtdDisciplinas;
+    int matricula;
+    char sexo[20];
+    int dia, mes, ano;
+    char cpf[15];
 } Professor;
 
-void cadastrarProfessor(Professor professores[], int *qtdProfessores);
-void listarProfessores(Professor professores[], int qtdProfessores);
-void listarProfessoresPorSexo(Professor professores[], int qtdProfessores, char sexo);
-void listarProfessoresOrdenadosPorNome(Professor professores[], int qtdProfessores);
+void incluirProfessor(Professor profs[], int* qtd);
+void listarProfessores(Professor profs[], int qtd);
+void atualizarProfessor(Professor profs[], int qtd);
+void excluirProfessor(Professor profs[], int* qtd);
 
 #endif
+
