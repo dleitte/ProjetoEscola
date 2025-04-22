63cf3d79548ff0b845e9757ffd189e0f7b254a6b
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2025-04-22 13:22:12 -0300

    Update professor.h

--- professor.h
+++ professor.h
@@ -15,6 +15,10 @@ void incluirProfessor(Professor profs[], int* qtd);
 void listarProfessores(Professor profs[], int qtd);
 void atualizarProfessor(Professor profs[], int qtd);
 void excluirProfessor(Professor profs[], int* qtd);
+void listarProfessoresPorNascimento(Professor professores[], int qtdProf);
+void listarProfessoresPorSexo(Professor professores[], int qtdProf);
+void listarProfessoresPorNome(Professor professores[], int qtdProf);
+void listarProfAniversariantesMes(Professor professores[], int qtdProf, int mesEscolhido);
 
 #endif
 
