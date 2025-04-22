a38ad690518e74940d24bf8d842251618378e208
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2025-04-22 12:07:52 -0300

    Update aluno.h

--- aluno.h
+++ aluno.h
@@ -15,5 +15,11 @@ void incluirAluno(Aluno alunos[], int* qtd);
 void listarAlunos(Aluno alunos[], int qtd);
 void atualizarAluno(Aluno alunos[], int qtd);
 void excluirAluno(Aluno alunos[], int* qtd);
+void listarAlunosPorSexo(Aluno alunos[], int qtdAluno);
+void listarAlunosPorNome(Aluno alunos[], int qtdAluno);
+void listarAlunosPorNascimento(Aluno alunos[], int qtdAluno);
+void listarAlunoAniversariantesMes(Aluno alunos[], int qtdAluno, int mesEscolhido);
+void listarAniversariantesMes(Professor professores[], int qtdProf, Aluno alunos[], int qtdAluno);
+void buscarPorNomeSubstring(Aluno alunos[], int qtdAluno, Professor professores[], int qtdProf);
 
 #endif
