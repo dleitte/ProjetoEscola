9be38db1f0fbe1bff6ac1c1e28af53ab289c7c09
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2025-04-22 10:33:20 -0300

    Update aluno.c

--- aluno.c
+++ aluno.c
@@ -2,76 +2,73 @@
 #include <string.h>
 #include "aluno.h"
 
-// Função para cadastrar um aluno
-void cadastrarAluno(Aluno alunos[], int *qtdAlunos) {
-    Aluno aluno;
-    printf("Informe o ID do aluno: ");
-    scanf("%d", &aluno.id);
-    printf("Informe o nome do aluno: ");
-    scanf(" %[^\n]", aluno.nome);
-    printf("Informe a idade do aluno: ");
-    scanf("%d", &aluno.idade);
-    printf("Informe o sexo do aluno (M/F): ");
-    scanf(" %c", &aluno.sexo);
-    printf("Informe a data de nascimento do aluno (dd/mm/aaaa): ");
-    scanf("%s", aluno.dataNascimento);
-
-    aluno.qtdDisciplinas = 0;  // Inicializa a quantidade de disciplinas como 0
-
-    alunos[*qtdAlunos] = aluno;
-    (*qtdAlunos)++;
+void incluirAluno(Aluno alunos[], int* qtdAlunos) {
+    if (*qtdAlunos < TAM_ALUNO) {
+        Aluno novoAluno;
+        printf("\nNome do Aluno: ");
+        scanf(" %[^\n]s", novoAluno.nome);
+        printf("Sexo (M/F): ");
+        scanf(" %c", &novoAluno.sexo);
+        printf("Data de Nascimento (dd/mm/aaaa): ");
+        scanf(" %s", novoAluno.dataNascimento);
+        
+        alunos[*qtdAlunos] = novoAluno;
+        (*qtdAlunos)++;
+        printf("Aluno incluído com sucesso!\n");
+    } else {
+        printf("Limite de alunos atingido!\n");
+    }
 }
 
-// Função para listar todos os alunos
 void listarAlunos(Aluno alunos[], int qtdAlunos) {
-    for (int i = 0; i < qtdAlunos; i++) {
-        printf("ID: %d, Nome: %s, Idade: %d, Sexo: %c, Data de Nascimento: %s\n", 
-                alunos[i].id, alunos[i].nome, alunos[i].idade, alunos[i].sexo, alunos[i].dataNascimento);
-    }
-}
-
-// Função para buscar aluno pelo nome
-void buscarAlunoPorNome(Aluno alunos[], int qtdAlunos, const char* nome) {
-    for (int i = 0; i < qtdAlunos; i++) {
-        if (strstr(alunos[i].nome, nome) != NULL) { // Verifica se o nome contém a substring
-            printf("Aluno encontrado: %s\n", alunos[i].nome);
+    if (qtdAlunos == 0) {
+        printf("Nenhum aluno cadastrado.\n");
+    } else {
+        for (int i = 0; i < qtdAlunos; i++) {
+            printf("\nAluno %d:\n", i + 1);
+            printf("Nome: %s\n", alunos[i].nome);
+            printf("Sexo: %c\n", alunos[i].sexo);
+            printf("Data de Nascimento: %s\n", alunos[i].dataNascimento);
         }
     }
 }
 
-// Função para listar alunos por sexo
-void listarAlunosPorSexo(Aluno alunos[], int qtdAlunos, char sexo) {
-    for (int i = 0; i < qtdAlunos; i++) {
-        if (alunos[i].sexo == sexo) {
-            printf("Aluno: %s, Sexo: %c\n", alunos[i].nome, alunos[i].sexo);
+void atualizarAluno(Aluno alunos[], int qtdAlunos) {
+    int i;
+    char nome[50];
+    printf("\nDigite o nome do aluno para atualizar: ");
+    scanf(" %[^\n]s", nome);
+    
+    for (i = 0; i < qtdAlunos; i++) {
+        if (strcmp(alunos[i].nome, nome) == 0) {
+            printf("Novo nome: ");
+            scanf(" %[^\n]s", alunos[i].nome);
+            printf("Novo sexo (M/F): ");
+            scanf(" %c", &alunos[i].sexo);
+            printf("Nova data de nascimento (dd/mm/aaaa): ");
+            scanf(" %s", alunos[i].dataNascimento);
+            printf("Aluno atualizado com sucesso!\n");
+            return;
         }
     }
+    printf("Aluno não encontrado!\n");
 }
 
-// Função para listar alunos ordenados por nome
-void listarAlunosOrdenadosPorNome(Aluno alunos[], int qtdAlunos) {
-    for (int i = 0; i < qtdAlunos - 1; i++) {
-        for (int j = i + 1; j < qtdAlunos; j++) {
-            if (strcmp(alunos[i].nome, alunos[j].nome) > 0) {
-                Aluno temp = alunos[i];
-                alunos[i] = alunos[j];
-                alunos[j] = temp;
+void excluirAluno(Aluno alunos[], int* qtdAlunos) {
+    int i;
+    char nome[50];
+    printf("\nDigite o nome do aluno para excluir: ");
+    scanf(" %[^\n]s", nome);
+    
+    for (i = 0; i < *qtdAlunos; i++) {
+        if (strcmp(alunos[i].nome, nome) == 0) {
+            for (int j = i; j < *qtdAlunos - 1; j++) {
+                alunos[j] = alunos[j + 1];
             }
+            (*qtdAlunos)--;
+            printf("Aluno excluído com sucesso!\n");
+            return;
         }
     }
-    listarAlunos(alunos, qtdAlunos);
-}
-
-// Função para listar alunos ordenados por data de nascimento
-void listarAlunosOrdenadosPorData(Aluno alunos[], int qtdAlunos) {
-    for (int i = 0; i < qtdAlunos - 1; i++) {
-        for (int j = i + 1; j < qtdAlunos; j++) {
-            if (strcmp(alunos[i].dataNascimento, alunos[j].dataNascimento) > 0) {
-                Aluno temp = alunos[i];
-                alunos[i] = alunos[j];
-                alunos[j] = temp;
-            }
-        }
-    }
-    listarAlunos(alunos, qtdAlunos);
+    printf("Aluno não encontrado!\n");
 }
