c6096f5b6acb68ea14460cc13a6d5423d2acfb04
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2025-04-22 13:06:42 -0300

    Update disciplina.c

--- disciplina.c
+++ disciplina.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include "disciplina.h"
+#include "aluno.h"
+#include "professor.h"
 
 void incluirDisciplina(Disciplina disciplinas[], int* qtdDisc, Professor professores[], int qtdProf) {
     if (*qtdDisc >= TAM_DISCIPLINA) {
