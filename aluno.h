0848b38a367caab8dd7f2627bb52bc89bb7b186b
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2025-04-22 10:49:07 -0300

    Update aluno.h

--- aluno.h
+++ aluno.h
@@ -4,14 +4,16 @@
 #define TAM_ALUNO 3
 
 typedef struct {
-    char nome[50];
-    char sexo;
-    char dataNascimento[11]; // dd/mm/aaaa
+    char nome[100];
+    int matricula;
+    char sexo[20];
+    int dia, mes, ano;
+    char cpf[15];
 } Aluno;
 
-void incluirAluno(Aluno alunos[], int* qtdAlunos);
-void listarAlunos(Aluno alunos[], int qtdAlunos);
-void atualizarAluno(Aluno alunos[], int qtdAlunos);
-void excluirAluno(Aluno alunos[], int* qtdAlunos);
+void incluirAluno(Aluno alunos[], int* qtd);
+void listarAlunos(Aluno alunos[], int qtd);
+void atualizarAluno(Aluno alunos[], int qtd);
+void excluirAluno(Aluno alunos[], int* qtd);
 
 #endif
